b0de4f63845f606a2d7c4646194ac6ed83cc23ca
baikhsam <baikhsam@gmail.com>
2023-11-13 02:37:02 -0500

    add error 404 page for kanbas course page

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -14,80 +14,106 @@ import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/AssignmentEditor";
 import Grades from "./Grades";
-import { useDispatch, useSelector } from "react-redux";
 import { findCourseById } from "../Dashboard/client";
-import { useEffect } from "react";
-import { setCourse } from "../Dashboard/coursesReducer";
+import { useEffect, useState } from "react";
 
 function Courses() {
-	const dispatch = useDispatch();
 	const { courseId } = useParams();
+	const [course, setCourse] = useState({});
 	useEffect(() => {
-		findCourseById(courseId).then((course) => {
-			dispatch(setCourse(course));
-		});
-	}, [courseId, dispatch]);
+		findCourseById(courseId)
+			.then((course) => {
+				if (!course) {
+					console.error("Course not found");
+				} else {
+					setCourse(course);
+				}
+			})
+			.catch((error) => {
+				if (error.response && error.response.status === 404) {
+					console.error("Course not found (404 error)");
+				} else {
+					console.error("Error loading course", error);
+				}
+				setCourse(null);
+			});
+	}, [courseId]);
 
-	const course = useSelector((state) => state.coursesReducer.course);
 	const location = useLocation().pathname.split("/");
 	const inAssignments = location.includes("Assignments");
 	console.log(location);
 
-	return (
-		<div className="wd-courses-container d-flex flex-column">
-			<div className="wd-courses-header ms-3 mt-4">
-				<nav
-					className="wd-courses-breadcrumb ms-3 float-start"
-					aria-label="breadcrumb"
-				>
-					<ol className="breadcrumb">
-						<li>
-							<FaBars className="wd-icon mx-2" size={24} />
-						</li>
-						<li className="breadcrumb-item">
-							<Link to={`/Kanbas/Courses/${courseId}/Home`}>
-								{course.number}
-							</Link>
-						</li>
-						<li
-							className="breadcrumb-item active"
-							aria-current="page"
-						>
-							{location.slice(-1)}
-						</li>
-					</ol>
-				</nav>
-				{inAssignments && (
-					<button
-						type="button"
-						className="btn btn-sm btn-secondary mx-3 float-end"
+	if (course === null) {
+		return (
+			<div className="container">
+				<h1>Whoops... Looks like nothing is here!</h1>
+				<h3>We couldn't find that page!</h3>
+				<h5>Page Not Found</h5>
+			</div>
+		);
+	} else {
+		return (
+			<div className="wd-courses-container d-flex flex-column">
+				<div className="wd-courses-header ms-3 mt-4">
+					<nav
+						className="wd-courses-breadcrumb ms-3 float-start"
+						aria-label="breadcrumb"
 					>
-						<FaGlasses className="me-2 mb-1" />
-						Student View
-					</button>
-				)}
-			</div>
-			<hr className="ms-3" />
-			<div className="wd-courses-content-block d-flex flex-row ms-3">
-				<div className="wd-courses-content-left">
-					<CourseNavigation />
+						<ol className="breadcrumb">
+							<li>
+								<FaBars className="wd-icon mx-2" size={24} />
+							</li>
+							<li className="breadcrumb-item">
+								<Link to={`/Kanbas/Courses/${courseId}/Home`}>
+									{course ? course.number : "Course"}
+								</Link>
+							</li>
+							<li
+								className="breadcrumb-item active"
+								aria-current="page"
+							>
+								{location.slice(-1)}
+							</li>
+						</ol>
+					</nav>
+					{inAssignments && (
+						<button
+							type="button"
+							className="btn btn-sm btn-secondary mx-3 float-end"
+						>
+							<FaGlasses className="me-2 mb-1" />
+							Student View
+						</button>
+					)}
 				</div>
-				<div className="wd-module d-flex flex-row">
-					<Routes>
-						<Route path="/" element={<Navigate to="Home" />} />
-						<Route path="Home" element={<Home />} />
-						<Route path="Modules" element={<Modules />} />
-						<Route path="Assignments" element={<Assignments />} />
-						<Route
-							path="Assignments/:assignmentId"
-							element={<AssignmentEditor />}
-						/>
-						<Route path="Assignments/create" element={<AssignmentEditor />} />
-						<Route path="Grades" element={<Grades />} />
-					</Routes>
+				<hr className="ms-3" />
+				<div className="wd-courses-content-block d-flex flex-row ms-3">
+					<div className="wd-courses-content-left">
+						<CourseNavigation />
+					</div>
+					<div className="wd-module d-flex flex-row">
+						<Routes>
+							<Route path="/" element={<Navigate to="Home" />} />
+							<Route path="Home" element={<Home />} />
+							<Route path="Modules" element={<Modules />} />
+							<Route
+								path="Assignments"
+								element={<Assignments />}
+							/>
+							<Route
+								path="Assignments/:assignmentId"
+								element={<AssignmentEditor />}
+							/>
+							<Route
+								path="Assignments/create"
+								element={<AssignmentEditor />}
+							/>
+							<Route path="Grades" element={<Grades />} />
+						</Routes>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 export default Courses;
